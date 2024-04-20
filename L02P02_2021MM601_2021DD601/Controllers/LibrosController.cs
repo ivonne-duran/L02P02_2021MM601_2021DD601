bc9773b83719a71395cfd3d4e7fc079dfeb558ed
Ivonne Duran <ivonne.duran@catolica.edu.sv>
2024-04-20 16:59:07 -0600

    Libros

---
+++ L02P02_2021MM601_2021DD601/Controllers/LibrosController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using L02P02_2021MM601_2021DD601.Models;
+
+namespace L02P02_2021MM601_2021DD601.Controllers
+{
+    public class LibrosController : Controller
+    {
+        private readonly librosDBContext _context;
+
+        public LibrosController(librosDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Libros
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.libros.ToListAsync());
+        }
+
+        // GET: Libros/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libros = await _context.libros
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (libros == null)
+            {
+                return NotFound();
+            }
+
+            return View(libros);
+        }
+
+        // GET: Libros/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Libros/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,libro,descripcion,url_imagen,id_autor,id_categoria,precio,estado")] Libros libros)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(libros);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(libros);
+        }
+
+        // GET: Libros/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libros = await _context.libros.FindAsync(id);
+            if (libros == null)
+            {
+                return NotFound();
+            }
+            return View(libros);
+        }
+
+        // POST: Libros/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,libro,descripcion,url_imagen,id_autor,id_categoria,precio,estado")] Libros libros)
+        {
+            if (id != libros.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(libros);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LibrosExists(libros.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(libros);
+        }
+
+        // GET: Libros/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var libros = await _context.libros
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (libros == null)
+            {
+                return NotFound();
+            }
+
+            return View(libros);
+        }
+
+        // POST: Libros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var libros = await _context.libros.FindAsync(id);
+            if (libros != null)
+            {
+                _context.libros.Remove(libros);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        [Route("Libros/GetByAutor/{idAutor}")]
+        public async Task<IActionResult> GetByAutor(int? idAutor)
+        {
+            if (idAutor == null)
+            {
+                return RedirectToAction(nameof(Index), "Autores");
+            }
+
+            var libros = await _context.libros.FirstOrDefaultAsync(libro => libro.id_autor == idAutor);
+
+            if (libros == null)
+            {
+                return RedirectToAction(nameof(Index), "Autores");
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LibrosExists(int id)
+        {
+            return _context.libros.Any(e => e.id == id);
+        }
+    }
+}
---
+++ L02P02_2021MM601_2021DD601/Models/Libros.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace L02P02_2021MM601_2021DD601.Models
+{
+    public class Libros
+    {
+        [Key]
+        [Display(Name = "ID")]
+        public int id { get; set; }
+        [Display(Name = "nombre del libro")]
+        public string nombre { get; set; }
+        [Display(Name = "descripcion")]
+        public string descripcion { get; set; }
+        [Display(Name = "url_imagen")]
+        public string url_imagen { get; set; }
+        [Display(Name = "id_autor")]
+        public int id_autor { get; set; }
+        [Display(Name = "id_categoria")]
+        public int id_categoria { get; set; }
+        [Display(Name = "precio")]
+        public decimal precio { get; set; }
+        [Display(Name = "estado")]
+        public string estado { get; set; }
+    }
+}
