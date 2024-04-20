f14ebe1e3258c72d22d5aecae21a260061c395f7
Ivonne Duran <ivonne.duran@catolica.edu.sv>
2024-04-20 15:32:50 -0600

    Ajustes y vista de autores

--- L02P02_2021MM601_2021DD601/Program.cs
+++ L02P02_2021MM601_2021DD601/Program.cs
@@ -1,7 +1,16 @@
+using L02P02_2021MM601_2021DD601.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<librosDBContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("libreriaDb")
+                )
+);
 
 var app = builder.Build();
 
