018337a407a9a7b3f44dd9f9374b5adabfcb19ba
Ivonne Duran <ivonne.duran@catolica.edu.sv>
2024-04-20 14:09:22 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2021MM601_2021DD601/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2021MM601_2021DD601.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
